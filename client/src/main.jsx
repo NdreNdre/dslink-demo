949c3c88fb9581d9e765025629fbb59a5033c7d8
NdreNdre <andresahulata08@gmail.com>
2026-03-11 21:34:54 +0700

    revisi 4

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import "@fontsource/dm-sans/300.css";
+import "@fontsource/dm-sans/400.css";
+import "@fontsource/dm-sans/500.css";
+import "@fontsource/dm-sans/600.css";
+import "@fontsource/dm-sans/700.css";
+import "@fontsource/source-serif-4/400.css";
+import "@fontsource/source-serif-4/600.css";
+import "@fontsource/source-serif-4/700.css";
+import "@fontsource/source-serif-4/900.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
